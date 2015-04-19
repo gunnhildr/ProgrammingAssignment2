b6468bea7e2fb4fc2b00448c4a575ddd852257eb
gunnhildr <trufaldinka@gmail.com>
2015-04-19 13:05:59 +0300

    Initial commit of programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,86 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This function generally creates a special "matrix" object that can cache its 
+# inverse.
+# More precise makecachematrix function creates a list containing functions to:
+# set the value of the matrix
+# get the value of the matrix
+# set the value of the inversed matrix
+# get the value of the inversed matrix
 
-## Write a short comment describing this function
+makecachematrix <- function(x=matrix()) {
+    z <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        z <<- NULL
+    }
+    
+    get <- function() {
+        x
+    }
+    
+    setInverseMatrix <- function(invertedMatrix){
+        z <<- invertedMatrix
+    }
+    
+    getInverseMatrix <- function() {
+        z
+    }
+    
+    list(set = set, get = get,
+         setInverseMatrix = setInverseMatrix,
+         getInverseMatrix = getInverseMatrix)
+}
 
-makeCacheMatrix <- function(x = matrix()) {
+# This function computes the inverse of the special "matrix" returned by 
+# makecachematrix above. If the inverse has already been calculated
+# (and the matrix has not changed), then the cachesolve should retrieve the 
+# inverse from the cache.
 
+cachesolve <- function(x,...) {
+    z <- x$getInverseMatrix()
+    if(!is.null(z)) {
+        message("getting cached data")
+        return(z)
+    }
+    data <- x$get()
+    z <- solve(data)
+    x$setInverseMatrix(z)
+    z
+}
+
+###############################################################################
+
+## Tests
+
+# Test basic caching
+# PASS if function returns message "Cached version matches solved version"
+# FAIL if function returns message "Cached version does not match solved version"
+
+testBasicCaching <- function(){
+    message<-"Cached version matches solved version"
+    n <- 3
+    mat <- matrix(rnorm(1:(n*n)), nrow=n, ncol=n)
+    matCached <- makecachematrix(mat)
+    matSolved1 <- cachesolve(matCached)
+    matSolved2 <- cachesolve(matCached)
+    if (!identical(matSolved1, matSolved2))
+        message<-message("Cached version does not match solved version")
+    message
 }
 
 
-## Write a short comment describing this function
+# Use a time test to see if we really save time
+# PASS if function returns message "Solve time is more or equal than cache time"
+# FAIL if function returns message "Solve time is less than cache time"
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+timeTest <- function(){
+    message<-"Solve time is more or equal than cache time"
+    n <- 128
+    mat <- matrix(rnorm(1:(n*n)), nrow=n, ncol=n)
+    matCached <- makecachematrix(mat)
+    time1 <- system.time(matSolved1 <- cachesolve(matCached))
+    time2 <- system.time(matSolved2 <- cachesolve(matCached))
+    if (time1["user.self"] < time2["user.self"])
+        message<-message("Solve time is less than cache time")
+    message
 }
